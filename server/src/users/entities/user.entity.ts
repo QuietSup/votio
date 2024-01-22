984b48d48f10846e9f87bdafaca686607f83f40b
QuietSup <feeling_of_infinity@outlook.com>
2024-01-22 11:05:13 +0200

    feat(server): add columns

--- server/src/users/entities/user.entity.ts
+++ server/src/users/entities/user.entity.ts
@@ -1,1 +1,23 @@
-export class User {}
+import { TimeRecord } from 'src/parent-entities/time-record.entity';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User extends TimeRecord {
+  @PrimaryGeneratedColumn('uuid')
+  uuid: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ nullable: true })
+  firstname: string;
+
+  @Column({ nullable: true })
+  lastname: string;
+
+  @Column()
+  password: string;
+}
