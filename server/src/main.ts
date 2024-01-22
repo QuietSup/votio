be188118bfda0409c264b29ea835ec2ccac129fc
QuietSup <feeling_of_infinity@outlook.com>
2024-01-22 10:32:06 +0200

    feat(server): add global prefix 'api'

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,7 @@ import { appConfig } from './config/app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   await app.listen(appConfig.port);
 }
 bootstrap();
