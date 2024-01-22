bb49bac58e7fc3f33defcc4f87fcf2738a48abf6
QuietSup <feeling_of_infinity@outlook.com>
2024-01-22 20:39:11 +0200

    feat(server): add User to the list of db entities

--- server/src/config/db.config.ts
+++ server/src/config/db.config.ts
@@ -1,4 +1,5 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/modules/users/entities/user.entity';
 
 export const dbConfigModule = TypeOrmModule.forRoot({
   type: 'postgres',
@@ -6,7 +7,7 @@ export const dbConfigModule = TypeOrmModule.forRoot({
   port: Number(process.env.DB_PORT),
   password: process.env.DB_PASSWORD,
   username: process.env.DB_USERNAME,
-  entities: [],
+  entities: [User],
   database: process.env.DB_NAME,
   synchronize: true,
   // logging: true,
