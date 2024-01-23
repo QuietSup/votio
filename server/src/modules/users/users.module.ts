a1e57f9cc575d4f338c0a98d213b5f42b313748f
QuietSup <feeling_of_infinity@outlook.com>
2024-01-23 17:09:03 +0200

    fix(server): import typeorm module

--- server/src/modules/users/users.module.ts
+++ server/src/modules/users/users.module.ts
@@ -3,9 +3,11 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { CryptoModule } from '../crypto/crypto.module';
 import { ExcludePasswordInterceptor } from './interceptors/exclude-password.interceptor';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 @Module({
-  imports: [CryptoModule],
+  imports: [CryptoModule, TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService, ExcludePasswordInterceptor],
 })
