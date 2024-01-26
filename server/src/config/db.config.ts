63478c34c5fb431764947eec366c7180f27ae960
QuietSup <feeling_of_infinity@outlook.com>
2024-01-26 13:22:20 +0200

    fix(server): add entities to db config

--- server/src/config/db.config.ts
+++ server/src/config/db.config.ts
@@ -1,4 +1,8 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PollAnswer } from 'src/modules/poll-answers/entities/poll-answer.entity';
+import { PollsAnswerOption } from 'src/modules/polls-answer-options/entities/polls-answer-option.entity';
+import { Poll } from 'src/modules/polls/entities/poll.entity';
+import { Survey } from 'src/modules/surveys/entities/survey.entity';
 import { User } from 'src/modules/users/entities/user.entity';
 
 export const dbConfigModule = TypeOrmModule.forRoot({
@@ -7,7 +11,7 @@ export const dbConfigModule = TypeOrmModule.forRoot({
   port: Number(process.env.DB_PORT),
   password: process.env.DB_PASSWORD,
   username: process.env.DB_USERNAME,
-  entities: [User],
+  entities: [User, Survey, Poll, PollAnswer, PollsAnswerOption],
   database: process.env.DB_NAME,
   synchronize: true,
   // logging: true,
